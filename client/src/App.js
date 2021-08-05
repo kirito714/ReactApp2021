5fd0018c182e6ea2dd55140f935100aeb5b084fe
methom21 <mikey.thompson8@gmail.com>
2021-08-04 20:57:26 -0700

    got the route from the homepage tothe sign in page working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Navbar from "./components/Navbar";
-import Signupform from "./components/Signupform";
-
+import Signup from "./pages/SignupPage";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <div className="App">
-      {/* <ApolloProvider> */}
+    //  <ApolloProvider>
+    <Router>
       <Navbar />
-      <Signupform />
-         {/* <Route  exact path="/" component={}/>
-        <Route  exact path="/" component={} /> */}
-        {/* <Footer/> */}
+      <Container />
+      {/* <Signupform /> */}
+      <Route exact path="/Signup">
+        <Signup />
+      </Route>
+      {/*<Route  exact path="/" component={} /> */}
+      {/* <Footer/> */}
       {/* <script */}
-     
-    {/* </ApolloProvider> */}
-    </div>
+    </Router>
+    //  </ApolloProvider>
   );
 }
 
