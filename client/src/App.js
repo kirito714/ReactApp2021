76052f0d7cd1eb8cb71ca04188dfa6e3e3388b4b
methom21 <mikey.thompson8@gmail.com>
2021-08-04 18:55:11 -0700

    added sign up form/ log in form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Signupform from "./components/Signupform";
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      <Navbar />
+      <Signupform />
+      <header className="App-header"></header>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </div>
   );
