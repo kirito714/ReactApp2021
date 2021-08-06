d2d27ca3b0e42f3d27b8dbc4093531cdce174352
Joseph Amador <kirito.1986.714@gmail.com>
2021-08-06 14:13:59 -0700

    merge

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -3,22 +3,9 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '60ch',
-    },
-  },
-}));
 
 // signUp for that handles formState
 export default function SignupPage() {
-  const classes = useStyles();
   const [formState, setFormState] = useState({
     username: "",
     email: "",
@@ -50,68 +37,42 @@ export default function SignupPage() {
     }
   };
   return (
-    <>
-          <h1>Sign Up Page!</h1>
-          
-          <Container maxWidth="sm">
-          <h2>Sign Up</h2>
+    <main >
+      <div>
+        <div>
+          <h4>Sign Up</h4>
+          <div>
             {data ? (
               <p>
-                Success! You may now head{'./Profile '}
+                Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
-              <form className={classes.root} noValidate autoComplete="off" onSubmit={handleFormSubmit}>
-                <div>
-                  <TextField
-                  // name="username"
-                  // type="text"
-                  id="filled-username-input"
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your username"
                   name="username"
-                  label="Your username"
-                  type="username"
-                  autoComplete="current-username"
-                  variant="filled"
-                  // value={formState.name}
+                  type="text"
                   value={formState.name}
                   onChange={handleChange}
-                >
-                  </TextField>
-                  </div>
-
-                  <div>
-                <TextField    
-                  // className="form-input"
-                  // name="email"
-                  // type="email"
-                  id="filled-email-input"
+                />
+                <input
+                  className="form-input"
+                  placeholder="Your email"
                   name="email"
-                  label="Your email"
                   type="email"
-                  autoComplete="current-email"
-                  variant="filled"
-                  // value={formState.email}
-                  value={formState.name}
+                  value={formState.email}
                   onChange={handleChange}
                 />
-                </div>
-
-                <div>
-                  <TextField
-                  // className="form-input"
-                  // name="password"
-                  // type="password"
-                  id="filled-password-input"
+                <input
+                  className="form-input"
+                  placeholder="******"
                   name="password"
-                  label="******"
                   type="password"
-                  autoComplete="current-password"
-                  variant="filled"
-                  // value={formState.password}
-                  value={formState.name}
+                  value={formState.password}
                   onChange={handleChange}
                 />
-                </div>
                 <button
                   className="btn btn-block btn-primary"
                   style={{ cursor: 'pointer' }}
@@ -127,7 +88,9 @@ export default function SignupPage() {
                 {error.message}
               </div>
             )}
-            </Container>
-    </>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
