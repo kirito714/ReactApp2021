338892c8f3afcbb94aec25bc85f5a7fd7efb6ee2
Joseph Amador <kirito.1986.714@gmail.com>
2021-08-08 17:41:27 -0700

    aded searchPage

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -7,6 +7,7 @@ export default function SearchPage() {
         <div className="input-field col s12">
           <textarea id="textarea1" className="materialize-textarea"></textarea>
           <label for="textarea1">Search for events!</label>
+          <label for="textarea1">Curly is Awesome!!</label>
         </div>
       </div>
     </form>
