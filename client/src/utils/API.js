18a980aa14880d89296d5be5d4f3a0785821016d
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-08-09 13:27:57 -0700

    move api keys to .env

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,51 +10,34 @@ async function SearchConcertData(city) {
 
   // const artistName = artist.toLowerCase().split(" ").join("+");
 
-  const apiKey = "72a52a72e7a14c1a47a69d46ea5e7322";
   const openWeatherParams = new URLSearchParams({
     q: `${cityName}`,
     units: "imperial",
-    appid: `${apiKey}`,
   }).toString();
 
-  const data = await (
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?${openWeatherParams}`
-    )
-  ).json();
+  const data = await (await fetch(`/weather?${openWeatherParams}`)).json();
 
   const lat = data.coord.lat;
   const lon = data.coord.lon;
   const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   const yyyy = today.getFullYear();
 
-  const todaysDate = yyyy + '-' + mm + '-' + dd;
-  console.log(today);
-
-
+  const todaysDate = yyyy + "-" + mm + "-" + dd;
 
   const predictHQParams = new URLSearchParams({
     category: "concerts",
     country: "US",
     limit: "10",
     "location_around.origin": `${lat},${lon}`,
-    "within": "10000m@" + `${lat},${lon}`,
+    within: "10000m@" + `${lat},${lon}`,
     sort: "start",
     "start.gt": `${todaysDate}`,
   }).toString();
 
   const res = await (
-    await fetch(
-      `https://api.predicthq.com/v1/events?${predictHQParams}`,
-      {
-        headers: {
-          Authorization:
-            "Bearer UCPQh7iuieqQpZWV4NOqoJPMhKvavlBNoAfad84K",
-        },
-      }
-    )
+    await fetch(`https://api.predicthq.com/v1/events?${predictHQParams}`)
   ).json();
   return res;
 }
